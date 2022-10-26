dfe5333d5cec2b9dfd4db78421be154a02df4d84
Aneika11 <aneikananton@gmail.com>
2022-10-26 18:27:08 -0400

    update: make products page

--- app.js
+++ app.js
@@ -4,13 +4,14 @@ let cors = require('cors')
 app.use(cors())
 const {Pool} = require('pg')
 const pool = require('./dbconfig.js')
+const productControl = require('./Controllers/productscontrollers')
 
 app.use(express.json())
 
 const PORT = process.env.PORT || 8000;
 
 
-app.get('/products',productscotrollers.getProducts)
+app.get('/products',productControl.getProducts)
 
 
 app.listen(PORT, () => {
