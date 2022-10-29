1153900a4e8b5c2928519b6d071491b5ede385dd
josemazhuvanchery1 <jose1998mathew@gmail.com>
2022-10-29 15:22:15 -0400

    fix: store redirect link

--- Front-End/marcymerch/js/login.js
+++ Front-End/marcymerch/js/login.js
@@ -19,7 +19,7 @@ async function checkLogin(event){
         let data = await fetch("http://localhost:8000/customers/login", postRequest)
         let userObj = await data.json()
         console.log(userObj)
-        if(userObj.message === "user not found") alert("Account already exists! Please log in")
+        if(userObj.message === "user not found") alert("Account does not exist! Please sign up")
         else if (userObj.message === "wrong password") console.log("Wrong password or username")
         else if(userObj.user_id) window.location.href = "http://127.0.0.1:5501/Products_files/index.html";
     }
