903a9493928cab2d92101793ed2c6dab562101a3
Aneika11 <aneikananton@gmail.com>
2022-10-28 22:35:00 -0400

    connected homepage to shopping page

--- seeds/products.js
+++ seeds/products.js
@@ -6,14 +6,14 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('products').del()
   await knex('products').insert([
-    {product_name: 'Fancy Grey Hoodie', price: 60.00, quantity: 3, image:"../Products_files/assets/grey_thumbnail.png"},
+    {product_name: 'Fancy Nude Hoodie', price: 60.00, quantity: 3, image:"../Products_files/assets/product1.png"},
     {product_name: 'Fall Green Sweatshirt', price: 56.00, quantity: 3, image:"../Products_files/assets/greensweat.jpg"},
-    {product_name: 'Stripe RedSweatpants', price: 70.00, quantity: 3, image:"../Products_files/assets/stripehood.jpg"},
-    {product_name: 'White Bagpack', price: 56.00, quantity: 3, image:"../Products_files/assets/WhiteBag.jpg"},
-    {product_name: 'Warm Purple Bagpack', price: 56.00, quantity: 3, image:"../Products_files/assets/purpleBag_thumbnail.png"},
+    {product_name: 'Grey Hoodie', price: 70.00, quantity: 3, image:"../Products_files/assets/Product.png"},
+    {product_name: 'White Bag pack', price: 56.00, quantity: 3, image:"../Products_files/assets/WhiteBag.jpg"},
+    {product_name: 'Warm Purple Bag pack', price: 56.00, quantity: 3, image:"../Products_files/assets/purpleBag_thumbnail.png"},
     {product_name: 'Everyday Tote Bag', price: 56.00, quantity: 3, image:"../Products_files/assets/ToteBag_thumbnail.png"},
-    {product_name: 'Orange Beanie', price: 56.00, quantity: 3, image:"../Products_files/assets/Beenie_thumbnail.png"},
-    {product_name: 'Yellow Beanie', price: 56.00, quantity: 3, image:"../Products_files/assets/yellowB.jpg"}
+    {product_name: 'Loose Fitted Orange Beanie', price: 56.00, quantity: 3, image:"../Products_files/assets/Beenie_thumbnail.png"},
+    {product_name: 'Fitted Yellow Beanie', price: 56.00, quantity: 3, image:"../Products_files/assets/yellowB.jpg"}
   ]);
   
 };
